43a5d7c3be4fc149fd9bb3d1d1822624f78a83cf
jasbel <asbel.elmec@gmail.com>
2022-07-20 15:06:09 -0400

    feat: small changes

---
+++ src/lazyload/layout/LazyLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const LazyLayout = () => {
+  return (
+    <div>
+      <h1>LazyLayout Page</h1>
+
+      {/* Rutas hijas aqui */}
+    </div>
+  )
+}
+
+export default LazyLayout
