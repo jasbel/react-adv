e8dc4ea01e83e94c87bf20fdddd50aeba9cc812b
jasbel <asbel.elmec@gmail.com>
2022-07-21 14:54:25 -0400

    feat:

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -12,14 +12,14 @@ interface IRoute {
 }
 
 const LazyLayout = lazy(() => import(/* webpackChunkName: "LazyLayout" */"../lazyload/layout/LazyLayout"));
-const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */"../lazyload/pages/LazyPage1"));
-const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */"../lazyload/pages/LazyPage2"));
-const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */"../lazyload/pages/LazyPage3"));
+// const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */"../lazyload/pages/LazyPage1"));
+// const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */"../lazyload/pages/LazyPage2"));
+// const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */"../lazyload/pages/LazyPage3"));
 
 export const routes: IRoute[] = [
   // { to: "/lazy1", path: "lazy1", name: "lazy-1", Component: Lazy1 },
-  { to: "/lazy-layout/*", path: "lazy-layout", name: "lazyLayout", Component: LazyLayout },
+  { to: "/lazy-layout/", path: "/lazy-layout/*", name: "lazyLayout", Component: LazyLayout },
   { to: "/no-lazy", path: "no-lazy", name: "noLazy", Component: NoLazy },
-  { to: "/lazy2", path: "lazy2", name: "lazy-2", Component: Lazy2 },
-  { to: "/lazy3", path: "lazy3", name: "lazy-3", Component: Lazy3 },
+  // { to: "/lazy2", path: "lazy2", name: "lazy-2", Component: Lazy2 },
+  // { to: "/lazy3", path: "lazy3", name: "lazy-3", Component: Lazy3 },
 ];
